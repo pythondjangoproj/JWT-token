b6809f8c9d2cb5fa44bab6fb211fecc7029573de
vishal.pyhon10@gmail.com <91028128+pythondjangoproj@users.noreply.github.com>
2022-03-25 14:49:24 +0530

    third commit

--- jwt token/testapp/views.py
+++ jwt token/testapp/views.py
@@ -4,15 +4,15 @@ from .serializers import EmployeeSerializers
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser
-
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 # Create your views here.
 
 class EmployeeCRUDCBV(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializers
-    authentication_classes = [TokenAuthentication, ]
+    authentication_classes = [JSONWebTokenAuthentication, ]
     # permission_classes = [IsAuthenticated, ]
-    permission_classes = [IsAdminUser,]
+    permission_classes = [IsAuthenticated,]
     # ******to overside global setting only for this class , allowany authentication is used
     # permission_classes = [AllowAny, ]
